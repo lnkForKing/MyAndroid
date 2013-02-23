75cf28a9e3ad89afd067e089a6e7cbf8bd35cacc
Administrator <Administrator@PC-201208310012>
2013-02-23 17:24:59 +0800

    读取SD卡文件和用SAX解析XML文件

--- src/cn/king/myandroid/MainActivity.java
+++ src/cn/king/myandroid/MainActivity.java
@@ -1,11 +1,20 @@
 package cn.king.myandroid;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.util.List;
+
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.xml.sax.SAXException;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.Environment;
 import android.util.Log;
 import android.view.Menu;
 import android.view.View;
@@ -14,6 +23,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+import cn.king.jdo.Person;
 import cn.king.manager.FileUtil;
 
 public class MainActivity extends Activity {
@@ -31,8 +41,10 @@ public class MainActivity extends Activity {
 		Button saveBtn = getViewById(R.id.save);
 		Button appendBtn = getViewById(R.id.append);
 		Button showContextBtn = getViewById(R.id.showContext);
-		saveBtn.setOnClickListener(saveFileListener);
-		appendBtn.setOnClickListener(saveFileListener);
+		Button readXMLBtn = getViewById(R.id.readXML);
+		saveBtn.setOnClickListener(saveFileListener); //保存文件内容
+		appendBtn.setOnClickListener(saveFileListener); //追加文件内容
+		//显示文件内容
 		showContextBtn.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View v) {
@@ -52,8 +64,37 @@ public class MainActivity extends Activity {
 
 			}
 		});
+		//读取SD卡的XML文件
+		readXMLBtn.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				//判断内存是否读写
+				if(!Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED)){
+					Toast.makeText(MainActivity.this, "读不取内存卡或无权限", Toast.LENGTH_LONG);
+					return;
+				}
+				try {
+					File sdDir = Environment.getExternalStorageDirectory(); //sd卡的根目录
+					//读取sdDir文件夹下的myXML.xml文件
+					InputStream inStream = new FileInputStream(new File(sdDir,"myXML.xml"));
+					FileUtil fileUtil = new FileUtil();
+					List<Person> list = fileUtil.getPersonsByXML(inStream);
+					StringBuffer sb = new StringBuffer();
+					for (Person person : list) {
+						sb.append(person.toString() + "\r\n");
+					}
+					showContextTView.setText(sb);
+				} catch (Exception e) {
+					Log.i(TAG, e.toString());
+					Toast.makeText(MainActivity.this, R.string.readError, Toast.LENGTH_LONG).show();
+				}
+			}
+		});
 	}
 	
+	/**
+	 * 保存或追加文件内容
+	 */
 	private OnClickListener saveFileListener = new View.OnClickListener() {
 		@Override 
 		public void onClick(View v) {
@@ -68,9 +109,9 @@ public class MainActivity extends Activity {
 				try {
 					Button btn = (Button)v;
 					OutputStream outStream = null;
-					if(btn.getId() == R.id.save){
+					if(btn.getId() == R.id.save){  //保存内容
 						outStream = MainActivity.this.openFileOutput(fileName, MODE_PRIVATE);
-					}else if(btn.getId() == R.id.append){
+					}else if(btn.getId() == R.id.append){  //追加内容
 						outStream = MainActivity.this.openFileOutput(fileName, MODE_APPEND);
 					}
 					FileUtil.save(outStream, context);
